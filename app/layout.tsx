5b820e50d28d48595a29da4326f7d4b3bbb15c60
Jacksonmills <jacksonemills@gmail.com>
2023-08-12 22:03:00 -0500

    fix(layout): Update description in metadata
    - Updated the description in the metadata for the website "Get Ratioed!" to include information about the recent XqC vs. H3H3 drama on Twitter.
    
    feat(layout): Add analytics component
    
    - Added the <Analytics /> component to the layout of the website to enable tracking and analytics.
    
    fix(page): Update "VS" text
    
    - Updated the text "VS" to "VS." in the tweet comparison card to fix a typo.
    
    chore(deps): Add @vercel/analytics package
    
    - Added the "@vercel/analytics" package to the project dependencies.
    
    chore(deps): Update package.json and pnpm-lock.yaml
    
    - Added the "@vercel/analytics" package to the package.json and pnpm-lock.yaml files with the specified version.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from './(components)/ThemeProvider';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Get Ratioed!',
   description:
-    '"Get Ratioed!" enables users to visualize the ratio when a tweet is collectively ratioed on Twitter.',
+    '"Get Ratioed!" A Visualizer for Twitter Ratio\'s. Notably the recent XqC vs. H3H3 drama.',
 };
 
 export default function RootLayout({
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
