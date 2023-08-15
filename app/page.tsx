0f0d8f2721be40bb462e9ed2cb2b01358f5196bd
Jacksonmills <jacksonemills@gmail.com>
2023-08-15 09:48:03 -0500

    feat: Update tweet fetching logic for home page
    - Use `getBaseUrl` function from utils.ts to get the base URL for API requests.
    - Instead of directly calling the `/api` endpoint, now fetch tweet and quoted tweet using the base URL and tweet IDs.
    - Clean up commented-out code.
    
    Note: This commit updates the tweet fetching logic for the home page, leveraging the `getBaseUrl` function from utils.ts to determine the base URL for API requests. Instead of direct API calls, the code now uses the base URL along with the tweet IDs to fetch the tweet and its quoted tweet. The changes also involve removing commented-out code that was no longer needed.

--- app/page.tsx
+++ app/page.tsx
@@ -3,37 +3,24 @@ import Ratio from './(components)/Ratio';
 import TweetCard from './(components)/TweetCard';
 
 import { Space_Grotesk } from 'next/font/google';
+import { getBaseUrl } from '@/lib/utils';
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
 
 export default async function Home() {
-  // const tweet: TweetType | undefined = await getTweet(
-  //   '1690136127095934978'
-  // ).catch((e) => {
-  //   console.log(e);
-  //   return undefined;
-  // });
-
-  const tweet: TweetType = await fetch(`/api/1690136127095934978`).then(
-    (res) => {
-      if (res.ok) return res.json();
-      return undefined;
-    }
-  );
+  const { data: tweet } = await fetch(
+    `${getBaseUrl()}/1690136127095934978`
+  ).then((res) => {
+    if (res.ok) return res.json();
+    return undefined;
+  });
 
   if (!tweet) return <div>Failed to load tweet</div>;
 
   if (!tweet.quoted_tweet) return <div>Tweet has no quoted tweet</div>;
 
-  // const quotedTweet: TweetType | undefined = await getTweet(
-  //   tweet.quoted_tweet.id_str
-  // ).catch((e) => {
-  //   console.log(e);
-  //   return undefined;
-  // });
-
-  const quotedTweet: TweetType = await fetch(
-    `/api/${tweet.quoted_tweet.id_str}`
+  const { data: quotedTweet } = await fetch(
+    `${getBaseUrl()}/${tweet.quoted_tweet.id_str}`
   ).then((res) => {
     if (res.ok) return res.json();
     return undefined;
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getBaseUrl() {
+  return process.env.NODE_ENV === "development" ? 'http://localhost:3000/api' : process.env.NEXT_PUBLIC_API_BASE_URL;
+}
