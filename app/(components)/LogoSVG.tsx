6a235c123c1d049599ce6f389f608a663c3d9a41
Jacksonmills <jacksonemills@gmail.com>
2023-08-15 20:56:05 -0500

    feat(Logo): Add LogoSVG component
    - Added a new component called LogoSVG to handle the rendering of the logo SVG.
    - Updated the Logo component to use the new LogoSVG component.
    - Refactored the Logo component to optimize the SVG rendering.
    
    feat(MakeYourOwnRatio): Add open/close functionality
    
    - Added open/close functionality to the MakeYourOwnRatio component.
    - Added a new state variable called isOpen to control the visibility of the popover.
    - Added event listeners for mouse clicks outside the popover and pressing the Escape key to close the popover.
    
    feat(MakeYourOwnRatio): Update styling and button text
    
    - Updated the styling of the Generate button in the MakeYourOwnRatio component.
    - Updated the text of the Generate button to display "Close" when the popover is open and "Generate" when closed.
    
    feat(loading): Create loading component
    
    - Added a new loading component to display a spinning logo SVG while the page is loading.
    - Used the LogoSVG component in the loading component.

---
+++ app/(components)/LogoSVG.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function LogoSVG() {
+  return (
+    <div className="md:w-[3.2rem] md:h-[3.2rem] w-[2.6rem] h-[2.6rem]">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 194 194">
+        <circle className="fill-[#aa0900]" cx="96.56" cy="98" r="94.05" />
+        <path
+          className="fill-[#2a66ff]"
+          d="M141.6,180.56c-45.6,24.88-102.73,8.08-127.61-37.52C-10.88,97.44,5.92,40.31,51.52,15.44"
+        />
+        <path
+          className="fill-[#2a66ff]"
+          d="M186.8,124.52c-14.65,49.83-66.93,78.35-116.76,63.7C20.2,173.57-8.31,121.3,6.34,71.46"
+        />
+        <circle className="fill-[#071722]" cx="96.56" cy="98" r="23.27" />
+        <circle
+          className="fill-none stroke-[#071722]"
+          strokeMiterlimit="10"
+          strokeWidth="7"
+          cx="97"
+          cy="97"
+          r="93.5"
+        />
+      </svg>
+    </div>
+  );
+}
