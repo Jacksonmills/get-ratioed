3286e8f5e421e5fe32dea3c477d3247cb945cc84
Jacksonmills <jacksonemills@gmail.com>
2023-08-18 07:44:46 -0500

    feat(NavMenu): Add link to featured ratio
    This commit adds a new link to the NavMenu component that directs users to a specific ratio. The link is wrapped in a `Link` component from `next/link`.
    
    feat(ThemeToggle): Improve theme toggle functionality
    
    This commit improves the functionality of the ThemeToggle component. It now includes a tooltip that appears when the user hovers over the theme toggle button. Additionally, the logic for toggling between light, dark, and system themes has been enhanced.

--- app/(components)/NavMenu.tsx
+++ app/(components)/NavMenu.tsx
@@ -13,6 +13,7 @@ import {
 } from './ui/navigation-menu';
 import useWindow from '../(hooks)/useWindow';
 import { Menu } from 'lucide-react';
+import Link from 'next/link';
 
 export function NavMenu() {
   const { isMobile } = useWindow();
@@ -34,14 +35,17 @@ export function NavMenu() {
             <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
                 <NavigationMenuLink asChild>
-                  <div className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md">
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      Make your own!
+                  <Link href="/ratio/1691573305705422883-1691605741029998926">
+                    <div className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md">
+                      <div className="mb-2 mt-4 text-lg font-medium">
+                        Featured Ratio
+                      </div>
+                      <p className="text-sm leading-tight text-muted-foreground">
+                        {`"Ethan Klein 'Ratioed' Triumph: Calls Out Oliver Tree's
+                        Authenticity Shift in Twitter Showdown"`}
+                      </p>
                     </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Create your own custom tweet ratio comparison.
-                    </p>
-                  </div>
+                  </Link>
                 </NavigationMenuLink>
               </li>
               <ListItem href="/reply" title="Reply Example">
