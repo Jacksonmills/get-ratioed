445af613df6621d3b7a262996c7140785850e03c
Jacksonmills <jacksonemills@gmail.com>
2023-08-20 14:46:44 -0500

    Refactor RatioBar.tsx and add EmojiSplode component
    This commit simplifies the RatioBar component by extracting the EmojiSplode component for displaying different emojis based on certain conditions. The EmojiSplode component takes props like emoji, winningPercentage, targetPercentage, isTweetAWinner, and direction. It adds transitions and animations to the emojis, making them appear at different positions and with different effects based on conditional logic.

--- app/(components)/RatioBar.tsx
+++ app/(components)/RatioBar.tsx
@@ -28,15 +28,103 @@ export default function RatioBar({
         <span className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 h-full w-[1px] bg-current" />
       </div>
 
+      <EmojiSplode
+        emoji={`🤬`}
+        winningPercentage={
+          isTweetAWinner ? winningPercentage : losingPercentage
+        }
+        targetPercentage={targetPercentage}
+        isTweetAWinner={isTweetAWinner}
+        direction="top"
+      />
+      <EmojiSplode
+        emoji={`🔥`}
+        winningPercentage={
+          isTweetAWinner ? winningPercentage : losingPercentage
+        }
+        targetPercentage={targetPercentage}
+        isTweetAWinner={isTweetAWinner}
+        direction="middle"
+      />
+      <EmojiSplode
+        emoji={`👀`}
+        winningPercentage={
+          isTweetAWinner ? winningPercentage : losingPercentage
+        }
+        targetPercentage={targetPercentage}
+        isTweetAWinner={isTweetAWinner}
+        direction="bottom"
+      />
+    </div>
+  );
+}
+
+function EmojiSplode({
+  emoji,
+  winningPercentage,
+  targetPercentage,
+  isTweetAWinner,
+  direction = 'top',
+}: {
+  emoji: string;
+  winningPercentage: number;
+  targetPercentage: number;
+  isTweetAWinner: boolean;
+  direction?: 'top' | 'middle' | 'bottom';
+}) {
+  const randomDelay = ['300', '500', '700', '1000'];
+  const randomIndex = Math.floor(Math.random() * randomDelay.length);
+  let transformString = '';
+
+  switch (direction) {
+    case 'top':
+      transformString = isTweetAWinner
+        ? '-translate-y-full translate-x-1/2 rotate-45'
+        : '-translate-y-full -translate-x-1/2 -rotate-45';
+      break;
+    case 'middle':
+      transformString = isTweetAWinner
+        ? 'translate-y-none translate-x-full -rotate-360'
+        : 'translate-y-none -translate-x-full rotate-360';
+      break;
+    case 'bottom':
+      transformString = isTweetAWinner
+        ? 'translate-y-full translate-x-1/2 -rotate-45'
+        : 'translate-y-full -translate-x-1/2 rotate-45';
+      break;
+  }
+
+  return (
+    <div
+      className={`transition-transform delay-${
+        randomDelay[randomIndex]
+      } duration-1000 ${
+        winningPercentage === targetPercentage ? 'translate-y-[50px]' : ''
+      }`}
+    >
       <span
-        className={`emojiExplode absolute transition-all duration-200 delay-100 ease ${
-          winningPercentage !== targetPercentage && 'animate-none'
+        className={`absolute rotate-0 transition-all duration-200 delay-200 ease-out top-[-0.5em] left-0 ${
+          winningPercentage === targetPercentage
+            ? `opacity-100 scale-150 ${transformString}`
+            : 'opacity-0 scale-0'
         }`}
         style={{
-          transform: `scale(${winningPercentage === targetPercentage ? 1 : 0})`,
-          left: `calc(${winningPercentage}% - 0.5em)`,
+          left: isTweetAWinner ? `calc(${targetPercentage}% - 0.5em)` : 'unset',
+          right: isTweetAWinner
+            ? 'unset'
+            : `calc(${targetPercentage}% - 0.5em)`,
         }}
-      />
+      >
+        <span
+          className={`block transition-opacity ${
+            winningPercentage === targetPercentage
+              ? 'opacity-0 delay-500 duration-500 ease-out'
+              : 'opacity-100'
+          }`}
+        >
+          {emoji}
+        </span>
+      </span>
     </div>
   );
 }
